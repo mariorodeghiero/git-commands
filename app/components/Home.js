6655e77b8f1b6b11491eff6aa41ecf59752b5fef
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-07-19 23:47:30 -0300

    creating footer and box shortcut

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,91 @@
 // @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+// import {Link} from 'react-router-dom';
 import styles from './Home.scss';
 
 type Props = {};
 
-export default class Home extends Component<Props> {
-  props: Props;
+export default class Home extends Component < Props > {
+  // props : Props;
 
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Shortcut</h2>
-          <Link className="link-menu" to="/counter">Git</Link>
+          <p>v1.0.0</p>
+          <h2>Git Shortcut</h2>
+          <div className={styles.containerBox} data-tid="containerBox">
+            <div className={styles.box} data-tid="box">
+              <h3>Local repository</h3>
+              <hr />
+              <ul>
+                <li title="teste teste teste teste teste">git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+              </ul>
+            </div>
+            <div className={styles.box} data-tid="box">
+              <h3>Remote Repository</h3>
+              <hr />
+              <ul>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+              </ul>
+            </div>
+            <div className={styles.box} data-tid="box">
+              <h3>GitHub Repository</h3>
+              <hr />
+              <ul>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+              </ul>
+            </div>
+            <div className={styles.box} data-tid="box">
+              <h3>Branches</h3>
+              <hr />
+              <ul>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+              </ul>
+            </div>
+            <div className={styles.box} data-tid="box">
+              <h3>Remote Branches</h3>
+              <hr />
+              <ul>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+              </ul>
+            </div>
+            <div className={styles.box} data-tid="box">
+              <h3>Conflicts</h3>
+              <hr />
+              <ul>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+                <li>git commit -am</li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <footer>
+          <p>⚖️ MIT License</p>
+        </footer>
       </div>
     );
   }
