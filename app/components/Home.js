2fa9aaef9e0d9ce984ba3b4ba84950b5a2b7b7f2
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-07-21 10:46:22 -0300

    add git commands

--- app/components/Home.js
+++ app/components/Home.js
@@ -13,72 +13,69 @@ export default class Home extends Component < Props > {
       <div>
         <div className={styles.container} data-tid="container">
           <p>v1.0.0</p>
-          <h2>Git Shortcut</h2>
+          <h2>Git Cheat Sheet</h2>
           <div className={styles.containerBox} data-tid="containerBox">
             <div className={styles.box} data-tid="box">
-              <h3>Local repository</h3>
+              <h3>Configurations</h3>
               <hr />
               <ul>
-                <li title="teste teste teste teste teste">git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
+                <li title="teste teste teste teste teste" > git config --global user.name "your name"</li>
+                <li > git config --global user.email "your@email.com"</li>
+                <li > git config --global color.ui true </li>
+                <li > git config --list</li>
               </ul>
             </div>
             <div className={styles.box} data-tid="box">
-              <h3>Remote Repository</h3>
+              <h3>Starting a repository</h3>
               <hr />
               <ul>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
+                <li>git init</li>
+                <li>git status</li>
               </ul>
             </div>
             <div className={styles.box} data-tid="box">
-              <h3>GitHub Repository</h3>
+              <h3>Starting files</h3>
               <hr />
               <ul>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
+                <li>git add my_new_file.txt</li>
+                <li>git add my-file.ts another-file.js </li>
+                <li>git add .</li>
+                <li>git add --all</li>
+                <li> git rm --cached file-name.txt</li>
+                <li> git reset file-name.txt</li>
               </ul>
             </div>
             <div className={styles.box} data-tid="box">
-              <h3>Branches</h3>
+              <h3>Commiting</h3>
               <hr />
               <ul>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
+                <li>git commit -m "Add three files"</li>
+                <li>git commit -a -m "Do something once more"</li>
+                <li>git reset --soft HEAD^</li>
+                <li>git commit --amend -m "enter your message"</li>
               </ul>
             </div>
             <div className={styles.box} data-tid="box">
-              <h3>Remote Branches</h3>
+              <h3>Pulling and pushing</h3>
               <hr />
               <ul>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
+                <li>git remote add origin https://yourRepository.git
+                </li>
+                <li>git push -u origin master</li>
+                <li>git remote -v</li>
+                <li > git clone https://yourRepository.git</li>
+                <li>git pull</li>
               </ul>
             </div>
             <div className={styles.box} data-tid="box">
-              <h3>Conflicts</h3>
+              <h3>Branching</h3>
               <hr />
               <ul>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
-                <li>git commit -am</li>
+                <li>git branch</li>
+                <li>git branch branch-name</li>
+                <li>git checkout branch-name</li>
+                <li>git merge branch-name</li>
+                <li>git checkout -b branch-name</li>
               </ul>
             </div>
           </div>
